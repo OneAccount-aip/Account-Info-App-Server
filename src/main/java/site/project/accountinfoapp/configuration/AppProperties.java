fe8a907875a745e58006b90eecd13abe39787705
sungwon-097 <csw2757@gmail.com>
2023-03-02 15:04:28 +0900

    #1 환경변수 값을 Bean으로 등록

---
+++ src/main/java/site/project/accountinfoapp/configuration/AppProperties.java
@@ -0,0 +1,26 @@
+package site.project.accountinfoapp.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+public record AppProperties() {
+
+    @Data
+    @Component
+    @ConfigurationProperties(prefix = "app.oauth")
+    public static class Oauth{
+        String client_id;
+        String client_secret;
+        String scope;
+        String redirect_uri;
+    }
+
+    @Data
+    @Component
+    @ConfigurationProperties(prefix = "app.grant")
+    public static class GrantType{
+        String client_credentials;
+        String authorization_code;
+    }
+}
