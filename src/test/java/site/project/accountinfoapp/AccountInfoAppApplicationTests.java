37f164646d5ba6072f09752c60a8d3f357eacbb1
sungwon-097 <csw2757@gmail.com>
2023-03-02 14:58:57 +0900

    #1 프로젝트 생성

---
+++ src/test/java/site/project/accountinfoapp/AccountInfoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package site.project.accountinfoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountInfoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
