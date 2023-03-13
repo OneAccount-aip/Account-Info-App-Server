2f9c52a87b1743ad203ab8f50b92364094d399fd
sungwon-097 <csw2757@gmail.com>
2023-03-13 14:19:53 +0900

    [fix] Cors Issue

--- src/main/java/site/project/accountinfoapp/configuration/web/WebConfig.java
+++ src/main/java/site/project/accountinfoapp/configuration/web/WebConfig.java
@@ -1,22 +1,33 @@
 package site.project.accountinfoapp.configuration.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-import static site.project.accountinfoapp.common.component.AppProperties.*;
+import static site.project.accountinfoapp.common.component.AppProperties.Cors;
 
 @Configuration
 @RequiredArgsConstructor
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig{
 
     private final Cors cors;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://web-account-info-app-client-6g2llf5d1j9b.sel3.cloudtype.app/", "https://account-info-app-client.vercel.app/")
-                .allowedHeaders("Authorization");
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://www.bokjips.com/");
+        config.addAllowedOrigin("http://localhost:3000/");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addExposedHeader("Authorization");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
+
 }
